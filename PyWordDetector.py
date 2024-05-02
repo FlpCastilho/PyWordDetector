3559eea631dd46d6612036094bb926bb6c2723be
Felipe Castilho <fhscastilho@gmail.com>
2024-05-02 15:00:07 -0300

    Improved readability of code

--- PyWordDetector.py
+++ PyWordDetector.py
@@ -3,22 +3,17 @@ import os
 while True:
 
     with open('keywords.txt', 'r', encoding='utf-8') as keywords:
-        keywords = keywords.read()
-        keywords = keywords.lower()
-        keywords = keywords.split(",")
+        keywords = keywords.read().lower().split(",")
 
     txtContents = input('Provide path to file or paste text: ')
     if os.path.exists(txtContents):
         with open(txtContents, 'r', encoding='utf-8') as txtContents:
-            txtContents = txtContents.read().replace('\n', ' ')
-            txtContents = txtContents.lower()
-            txtContents = " " + txtContents
+            txtContents = " " + txtContents.read().replace('\n', ' ').lower()
             result = [i for i in keywords if(i in txtContents)]
             print('Does the text contain words from the keywords list: ' + str(bool(result)))
             continue
     else:
-            txtContents = txtContents.lower()
-            txtContents = " " + txtContents
+            txtContents = " " + txtContents.lower()
             result = [i for i in keywords if(i in txtContents)]
             print('Does the text contain words from the keywords list: ' + str(bool(result)))
             continue
